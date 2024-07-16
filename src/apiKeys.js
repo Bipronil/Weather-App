0e79face1451fdb069662fe7f21d6de010bfe241
unknown <bipronilghosh@gmail.com>
2024-07-16 18:20:59 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bcec16cd30b4ba5a9871c70210f038e8",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
